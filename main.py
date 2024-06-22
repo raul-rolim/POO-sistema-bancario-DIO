28679fa669e7b93270f822f2f082257b79ff5ba7
raul-rolim <rauldelucaroberto@gmail.com>
2024-06-21 21:06:47 -0300

    Correção no extrato e PEP8
    Corrigido a visualização do extrato e regularização do código seguindo conceitos de boas práticas estabelecidos pelo PEP8, através do recurso flake8, que pode ser utilizado no próprio terminal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Historico:
     def __init__(self):
         self._historico = ""
@@ -10,8 +11,9 @@ class Historico:
 
     def adicionar_transacao(self, transacao):
         self._historico += str(transacao)
-class Deposito:
 
+
+class Deposito:
     def __init__(self, valor):
         self._valor = valor
 
@@ -19,6 +21,7 @@ class Deposito:
     def valor(self):
         self._valor = float(input("Insira o valor que deseja depositar: "))
         return self._valor
+
 
 class Saque:
     def __init__(self, valor):
@@ -41,6 +44,7 @@ class Conta(ABC):
     @abstractmethod
     def sacar(self, valor):
         pass
+
 
 class ContaCorrente(Conta):
 
@@ -65,12 +69,13 @@ class ContaCorrente(Conta):
         deposito_valido = valor > 0
         if deposito_valido:
             self._saldo += valor
-            self._historico.adicionar_transacao(f"\nDepósito: R${self._saldo:.2f}")
+            self._historico.adicionar_transacao(f"\nDepósito: R${valor:.2f}")
             print("Depósito realizado com sucesso")
             return True
         else:
             print("O processo falhou, pois o valor inserido é inválido")
             return False
+
     @classmethod
     def nova_conta(cls, cliente, numero):
         print("Conta criada com sucesso")
@@ -82,7 +87,7 @@ class ContaCorrente(Conta):
 
         if saque_valido and limite_disponivel:
             self._saldo -= valor
-            self._historico.adicionar_transacao(f"\nSaque: R${self._saldo:.2f}")
+            self._historico.adicionar_transacao(f"\nSaque: R${valor:.2f}")
             print("Saque realizado com sucesso")
             return True
         elif not limite_disponivel:
@@ -91,11 +96,13 @@ class ContaCorrente(Conta):
         else:
             print("O valor inserido é inválido")
 
+
 class PessoaFisica(ABC):
     def __init__(self, nome, cpf, data_nascimento):
         self._nome = nome
         self._cpf = cpf
         self._data_nascimento = data_nascimento
+
 
 class Cliente(PessoaFisica):
     def __init__(self, nome, cpf, data_nascimento, endereco, senha):
@@ -131,8 +138,10 @@ class Cliente(PessoaFisica):
         return nome, cpf, data_nascimento, endereco, senha
 
     def __repr__(self):
-        return (f"Nome: {self._nome}, CPF: {self._cpf}, Nascimento: {self._data_nascimento}, "
+        return (f"Nome: {self._nome}, CPF: {self._cpf}, "
+                f"Nascimento: {self._data_nascimento}, "
                 f"Endereço: {self._endereco}, Senha: {self._senha}\n")
+
 
 class Usuarios:
     def __init__(self):
@@ -149,12 +158,16 @@ class Usuarios:
     @property
     def lista_usuarios(self):
         return self._lista_usuarios
+
+
 class Menu:
-    def menu_inicial(self):
+    @staticmethod
+    def menu_inicial():
         lista_de_usuarios = Usuarios()
         print("Menu principal")
         while True:
-            opcoes = "1 - Criar Usuário\n2 - Criar Conta Corrente\n3 - Fazer Login\n4 - Sair\nEscolha: "
+            opcoes = ("1 - Criar Usuário\n2 - Criar Conta Corrente\n"
+                      "3 - Fazer Login\n4 - Sair\nEscolha: ")
             escolha = int(input(opcoes))
             match escolha:
                 case 1:
@@ -164,46 +177,66 @@ class Menu:
                     data_nascimento = "01/01/2021"
                     endereco = "adqwqwdqw"
                     senha = "123456"
-                    novo_cliente = Cliente(nome, cpf, data_nascimento, endereco, senha)
+                    novo_cliente = Cliente(nome,
+                                           cpf,
+                                           data_nascimento,
+                                           endereco,
+                                           senha)
                     lista_de_usuarios.adicionar_usuario(novo_cliente)
 
                 case 2:
-                    nova_conta_corrente = ContaCorrente.nova_conta(lista_de_usuarios.lista_usuarios[0], len(lista_de_usuarios.lista_usuarios))
+                    nova_conta_corrente = ContaCorrente.nova_conta(
+                        lista_de_usuarios.lista_usuarios[0],
+                        len(lista_de_usuarios.lista_usuarios))
+
                     cliente = lista_de_usuarios.lista_usuarios[0]
                     cliente.adicionar_conta(nova_conta_corrente)
 
                 case 3:
                     cpf = input("Insira o CPF da sua conta: ")
                     senha = input("Insira sua senha: ")
-                    for conta in lista_de_usuarios.lista_usuarios:
-                        if conta.cpf == cpf and conta.senha == senha:
-                            print("Bem Vindo")
-                            menu.menu_principal(conta)
-
-                    print("Tente novamente")
+                    resultado = menu.verificar_conta(lista_de_usuarios,
+                                                     cpf,
+                                                     senha)
+                    if not resultado:
+                        print("Tente novamente")
 
                 case 4:
-                    print("Obrigado por utilizar o nosso serviço!\nAté a próxima!")
+                    print("Obrigado por utilizar o nosso serviço!\n"
+                          "Até a próxima!")
                     exit(0)
                 case 12:
                     print(lista_de_usuarios.listar())
-    def menu_principal(self, login):
-        menu_opcoes = "1 - Depositar\n2 - Sacar\n3 - Verificar Extrato\n4 - Sair\nEscolha: "
+
+    @staticmethod
+    def verificar_conta(lista_de_usuarios, cpf, senha):
+        for conta in lista_de_usuarios.lista_usuarios:
+            if conta.cpf == cpf and conta.senha == senha:
+                print("Bem Vindo")
+                menu.menu_principal(conta)
+                return True
+        return False
+
+    @staticmethod
+    def menu_principal(login):
+        menu_opcoes = ("1 - Depositar\n2 - Sacar\n3 - Verificar Extrato\n"
+                       "4 - Sair\nEscolha: ")
         while True:
             print(f"\nSaldo: R${login.contas[0].saldo:.2f}")
             opcao = int(input(menu_opcoes))
             match opcao:
                 case 1:
-                    valor = float(input("Insira o valor que deseja depositar: "))
+                    valor = float(input("Insira o valor: "))
                     login.contas[0].depositar(valor)
                 case 2:
-                    valor = float(input("Insira o valor que deseja sacar: "))
+                    valor = float(input("Insira o valor: "))
                     login.contas[0].sacar(valor)
                 case 3:
                     print(login.contas[0].historico.historico)
                 case 4:
                     print("Fim da Sessão")
                     break
+
 
 menu = Menu()
 menu.menu_inicial()
